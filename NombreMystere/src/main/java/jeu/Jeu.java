f9b5f67fbdef45005c96c501e1affc0a5a82057d
Jucrz <juliencruz78@gmail.com>
2021-03-26 15:55:01 +0100

    Ajout boucle principale

--- NombreMystere/src/main/java/jeu/Jeu.java
+++ NombreMystere/src/main/java/jeu/Jeu.java
@@ -1,10 +1,36 @@
 package jeu;
 
+import java.util.Scanner;
+
 public class Jeu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int choice = 0;
 		
+		do {			
+			System.out.println("1. Deviner le nombre mystère.");
+			System.out.println("2. Faire deviner le nombre mystère.");
+			System.out.println("3. Quitter l'application");
+			
+			Scanner sc = new Scanner(System.in);
+			choice = sc.nextInt();
+			
+			switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default :
+				System.out.println("Merci de saisir une valeur entre 1 et 3.");
+				break;
+			}
+			
+		} while(choice != 3);
+		
+		System.out.println("Fermeture de l'application.");
 	}
 
 }
