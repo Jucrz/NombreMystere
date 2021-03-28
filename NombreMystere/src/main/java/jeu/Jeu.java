c65c91ffc9e044cb15e6f61d02f48dbfa8ec630e
Jucrz <juliencruz78@gmail.com>
2021-03-28 23:38:39 +0200

    update jeu.java

--- NombreMystere/src/main/java/jeu/Jeu.java
+++ NombreMystere/src/main/java/jeu/Jeu.java
@@ -18,12 +18,12 @@ public class Jeu {
 				System.out.println("Que voulez vous faire ?");
 				System.out.println("1. Deviner le nombre mystère.");
 				System.out.println("2. Faire deviner le nombre mystère.");
-				System.out.println("3. Quitter l'application");
+				System.out.println("3. Quitter l'application.");
 				try {
 					choice = sc.nextInt();
 				} catch(Exception e) {
-					System.out.println("Vous devez taper un nombre a 4 chiffres.");
-				} 
+					System.out.println("Vous devez taper un chiffre.");
+				}
 			}
 			
 			switch (choice) {
